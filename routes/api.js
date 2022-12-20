156a500799070a04ee022ec4be13e602b4687daa
Yehuda Lehrfield <yll2113@columbia.edu>
2022-12-19 20:13:22 -0500

    install dependencies, eslint, mongodb connection

--- routes/api.js
+++ routes/api.js
@@ -2,46 +2,40 @@
 *
 *
 *       Complete the API routing below
-*       
-*       
+*
+*
 */
 
-'use strict';
-
 module.exports = function (app) {
-
   app.route('/api/books')
-    .get(function (req, res){
-      //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+    .get((req, res) => {
+      // response will be array of book objects
+      // json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
     })
-    
-    .post(function (req, res){
-      let title = req.body.title;
-      //response will contain new book object including atleast _id and title
+
+    .post((req, res) => {
+      const { title } = req.body;
+      // response will contain new book object including atleast _id and title
     })
-    
-    .delete(function(req, res){
-      //if successful response will be 'complete delete successful'
+
+    .delete((req, res) => {
+      // if successful response will be 'complete delete successful'
     });
-
-
 
   app.route('/api/books/:id')
-    .get(function (req, res){
-      let bookid = req.params.id;
-      //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+    .get((req, res) => {
+      const bookid = req.params.id;
+      // json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
-    
-    .post(function(req, res){
-      let bookid = req.params.id;
-      let comment = req.body.comment;
-      //json res format same as .get
+
+    .post((req, res) => {
+      const bookid = req.params.id;
+      const { comment } = req.body;
+      // json res format same as .get
     })
-    
-    .delete(function(req, res){
-      let bookid = req.params.id;
-      //if successful response will be 'delete successful'
+
+    .delete((req, res) => {
+      const bookid = req.params.id;
+      // if successful response will be 'delete successful'
     });
-  
 };
--- server.js
+++ server.js
@@ -1,56 +1,76 @@
-'use strict';
-
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const mongoose = require('mongoose');
+const apiRoutes = require('./routes/api');
+const fccTestingRoutes = require('./routes/fcctesting');
+const runner = require('./test-runner');
 
 const app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
+app.use(cors({ origin: '*' })); // USED FOR FCC TESTING PURPOSES ONLY!
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Index page (static HTML)
+// connect to mongodb via mongoose
+try {
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log('Connected to mongo');
+} catch {
+  console.log('Error: Could not connect to mongo');
+}
+
+// define new schema
+const bookSchema = new mongoose.Schema({
+  comments: [],
+  title: { type: String, required: true },
+  commentCount: { type: Number, default: 0 },
+});
+
+// create new model
+const book = mongoose.model('issue', bookSchema);
+
+// Index page (static HTML)
 app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  .get((req, res) => {
+    res.sendFile(`${process.cwd()}/views/index.html`);
   });
 
-//For FCC testing purposes
+// For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
-//404 Not Found Middleware
-app.use(function(req, res, next) {
+// Routing for API
+apiRoutes(app);
+
+// 404 Not Found Middleware
+app.use((req, res) => {
   res.status(404)
     .type('text')
     .send('Not Found');
 });
 
-//Start our server and tests!
-const listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
+// Start our server and tests!
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`);
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
-    setTimeout(function () {
+    setTimeout(() => {
       try {
         runner.run();
-      } catch(e) {
-          console.log('Tests are not valid:');
-          console.error(e);
+      } catch (e) {
+        console.log('Tests are not valid:');
+        console.error(e);
       }
     }, 1500);
   }
 });
 
-module.exports = app; //for unit/functional testing
+module.exports = app; // for unit/functional testing
